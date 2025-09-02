89b413b53a2a52af1e95111aa8fa2db7b1bbb3e3
Dayan-Parker <d.b.parker@wustl.edu>
2025-09-02 14:54:14 -0500

    adding read to the manger but there is still an error with wrDataD and wrAddrD

--- Axi4LiteRegs/Axi4LiteRegs.srcs/sources_1/new/Axi4LiteManager.sv
+++ Axi4LiteRegs/Axi4LiteRegs.srcs/sources_1/new/Axi4LiteManager.sv
@@ -54,39 +54,66 @@ module Axi4LiteManager #
         output logic    M_AXI_BREADY
     );
 
+// FSM State
+    //Read vs Write FSM
+    typedef enum logic [5:0] {IDLE, R, WR ,WR1,WR2,WRESP,RD1,RD2} statetype;
 
-    // FSM State
-    
-    typedef enum logic [3:0] {IDLE,WR1,WR2} statetype;
     
     statetype nextState, currState;
     
     //Write Flps
     
-    logic [ C_M_AXI_DATA_WIDTH-1:0] wrDataD, wrDataQ;
-    logic [C_M_AXI_ADDR_WIDTH-1:0] wrAddrD, wrAddrQ;
+    logic [ C_M_AXI_DATA_WIDTH-1:0] wrDataD, wrDataQ,rdDataD, rdDataQ;
+    logic [C_M_AXI_ADDR_WIDTH-1:0] wrAddrD, wrAddrQ, rdAddrD, rdAddrQ;
+    
+    //Create Delay 
+    logic [1:0] count = 0;
     
     always_ff @(posedge M_AXI_ACLK) begin
         if (!M_AXI_ARESETN) begin
             currState <= IDLE;
             wrDataQ <= 0;
             wrAddrQ <= 0;
+            rdAddrQ <= 0;
+            rdDataQ <= 0;
         end else begin
             currState <= nextState;
             wrDataQ <= wrDataD;
             wrAddrQ <= wrAddrD;
+            rdAddrQ <= rdAddrD;
+            rdDataQ <= rdDataD;
+
+        end
+        
+        //Counter increment
+        if (M_AXI_WREADY) begin
+            count <= count + 1;
         end
         
     end
     
     always_comb begin
         nextState = currState;
+        //write
         wrDataQ = wrDataD;
         wrAddrQ = wrAddrD;
         M_AXI_AWADDR=0;
         M_AXI_AWVALID =0;
         M_AXI_WDATA =0;
         M_AXI_WVALID =0;
+        M_AXI_BREADY =0;
+        count = 0;
+
+    
+        //read
+        M_AXI_ARADDR = rdAddrQ;
+        M_AXI_RREADY =0;
+        M_AXI_ARVALID =0;
+        rdAddrQ = rdAddrD;
+        rdDataQ = rdDataD;
+
+        
+        
         case (currState) 
             IDLE: begin
                 if (wr) begin
@@ -94,7 +121,29 @@ module Axi4LiteManager #
                     wrAddrD = wrAddr;
                     nextState = WR1;
                 end
+                else if (rd) begin
+                    rdAddrD = rdAddrQ;
+                    nextState = RD1;
+                end
             end
+            
+            //Read
+            RD1: begin
+                M_AXI_ARADDR = rdAddrQ;
+                M_AXI_RREADY = 1;
+                M_AXI_ARVALID = 1;
+                if (M_AXI_RVALID && M_AXI_ARREADY) begin
+                    nextState = RD2;
+                end
+            end
+            
+            RD2: begin
+                rdDataD = rdData;
+                
+            end
+            
+            
+            //Write
             WR1: begin
                 M_AXI_AWADDR = wrAddrQ;
                 M_AXI_AWVALID = 1;
@@ -105,7 +154,15 @@ module Axi4LiteManager #
                 end
             end
             WR2: begin
+                if (count >= 'h3) begin
+                    M_AXI_BREADY = 1;
+                    if (M_AXI_BVALID) nextState = WRESP;
+                end
             end
+            
+            WRESP: begin
+            end
+            
             default: begin
                 nextState = IDLE; 
             end
